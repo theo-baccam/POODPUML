b7e6ac94385869c834d5ffa40537e2418ff45c62
theo-baccam <theo.baccam@laplateforme.io>
2024-05-30 15:48:44 +0200

    test(test_MVC): check working controller and controller switching

---
+++ tests/test_MVC.cpp
@@ -0,0 +1,83 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <iostream>
+#include <string>
+#include <doctest.h>
+#include "Core.hpp"
+#include "ControllerInterface.hpp"
+#define NUM_VALUE 21
+
+
+class FirstView {
+    public:
+        FirstView() {};
+        ~FirstView() {};
+        int getNum(int num) {
+            return num;
+        }
+};
+
+class FirstModel {
+    public:
+        FirstModel() {};
+        ~FirstModel() {};
+        int num = NUM_VALUE;
+};
+
+class FirstController : public ControllerInterface {
+    public:
+        FirstController() {};
+        ~FirstController() {};
+
+        FirstView view;
+        FirstModel model;
+
+        void run() override {
+            std::cout << this->view.getNum(this->model.num) << '\n';
+        }
+};
+
+
+class SecondView {
+    public:
+        SecondView() {};
+        ~SecondView() {};
+
+        std::string getText(std::string text) {
+            return text;
+        }
+};
+
+class SecondModel {
+    public:
+        SecondModel() {};
+        ~SecondModel() {};
+
+        std::string text = "testing...";
+};
+
+class SecondController : public ControllerInterface {
+    public:
+        SecondController() {};
+        ~SecondController() {};
+
+        SecondView view;
+        SecondModel model;
+
+        void run() override {
+            std::cout << this->view.getText(this->model.text) << '\n';
+        }
+};
+
+
+TEST_CASE("Working Core and Controller classes") {
+    Core core(new FirstController);
+
+    SUBCASE("Running the current controller") {
+        core.currentController->run();
+    }
+
+    SUBCASE("Switching the current controller") {
+        core.switchController(new SecondController);
+        core.currentController->run();
+    }
+}
