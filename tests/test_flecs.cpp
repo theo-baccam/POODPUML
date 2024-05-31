a071a8f596e05060f1251d5513a0a16323bdc403
theo-baccam <theo.baccam@laplateforme.io>
2024-06-01 01:41:30 +0200

    refactor(test_flecs): added comments, cleaned up and tweaked tests

--- tests/test_flecs.cpp
+++ tests/test_flecs.cpp
@@ -4,73 +4,95 @@
 #include <flecs.h>
 
 
+// Used in: 4, 6
+typedef struct {double value;} Health;
+
+// Used in: 4
+typedef struct{
+    double x;
+    double y;
+} Position;
+
+// Used in: 5
+typedef struct {} StructTag;
+
+
 TEST_CASE("Working flecs framework") {
-    SUBCASE("Creating world") {
+
+    SUBCASE("1. Creating world") {
+        // The world is the container of all ECS data
+
         flecs::world world;
     }
 
-    SUBCASE("Creating entity") {
+    SUBCASE("2. Creating entity") {
+        // An entity is a unique ID in the world.
+        // They can have 0 to n IDs added to them.
+        // An ID is anything that can be added to an entity
+
         flecs::world world;
+
         auto johnDoe = world.entity();
         CHECK(johnDoe.is_alive() == true);
         johnDoe.destruct();
         CHECK(johnDoe.is_alive() == false);
     }
 
-    SUBCASE("Creating entity with name") {
+    SUBCASE("3. Create entity with name and look it up") {
+        // An entity can have a name in order to look it up easily
+
         flecs::world world;
+
         const char *name = "Jane Doe";
         auto janeDoe = world.entity(name);
         REQUIRE(janeDoe.is_alive() == true);
         CHECK(janeDoe.name() == name);
+
+        auto correctNameLookUp = world.lookup(name);
+        CHECK(janeDoe == correctNameLookUp);
+
+        auto wrongNameLookUp = world.lookup("John Doe");
+        CHECK(janeDoe != wrongNameLookUp);
+
         janeDoe.destruct();
     }
 
-    SUBCASE("Look up entity") {
-        flecs::world world;
-        const char *name = "Jan Duh";
-        auto janDuh = world.entity(name);
-        REQUIRE(janDuh.is_alive() == true);
-        auto lookedUpEntity = world.lookup(name);
-        CHECK(janDuh == lookedUpEntity);
-        auto mistypedEntity = world.lookup("Jlarp Dee");
-        CHECK(janDuh != mistypedEntity);
-        janDuh.destruct();
-    }
+    SUBCASE("4. Manage components") {
+        // A component is an entity which contains data
 
-    SUBCASE("Manage component") {
         flecs::world world;
-        auto jeefDing = world.entity("Jeef Ding");
 
-        typedef double Health;
+        auto johnDoe = world.entity();
+        REQUIRE(johnDoe.is_alive() == true);
+
+        // You can `add` a component to an entity without giving a value
+        // You can also `set`, which sets a component's value and also adds it
+        // if an entity doesn't already have it.
+
+        // By the way, a component can only be a struct or an entity,
+        // as far as I know, if you try to set multiple components as aliases
+        // of simpler types like int or double, the values will get mixed up.
+
         const double healthPoints = 15.28;
-        jeefDing.add<Health>();
-        jeefDing.set<Health>(healthPoints);
-        CHECK(*jeefDing.get<Health>() == healthPoints);
+        johnDoe.add<Health>();
+        johnDoe.set<Health>({healthPoints});
+        CHECK(johnDoe.has<Health>() == true);
+        CHECK(johnDoe.get<Health>()->value == healthPoints);
 
-        struct Position {
-            double x;
-            double y;
-        };
-        typedef struct Position Position;
         double x = 82.12;
         double y = -12.53;
-        jeefDing.set<Position>({x, y});
-        Position pos = *jeefDing.get<Position>();
-        CHECK(pos.x == x);
-        CHECK(pos.y == y);
-        jeefDing.destruct();
+        johnDoe.set<Position>({x, y});
+        CHECK(johnDoe.has<Position>() == true);
+
+        johnDoe.destruct();
     }
 
-    SUBCASE("Tag management") {
+    SUBCASE("5. Tag management") {
         // Tags are components with no data
 
         flecs::world world;
 
         // Struct Tag is fixed at compile time
-        struct StructTag {};
-        typedef struct StructTag StructTag;
-
         auto johnDoe = world.entity().add<StructTag>();
         CHECK(johnDoe.has<StructTag>() == true);
         johnDoe.remove<StructTag>();
@@ -83,23 +105,54 @@ TEST_CASE("Working flecs framework") {
         CHECK(janeDoe.has(EntityTag) == true);
         janeDoe.remove(EntityTag);
         CHECK(janeDoe.has(EntityTag) == false);
+
+        johnDoe.destruct();
+        janeDoe.destruct();
     }
 
-    SUBCASE("Simple each filter") {
+    SUBCASE("6. Simple each filter") {
+        // A filter is a query whose results are not cached, making it cheap
+        // to create.
+        // Useful when querying for something not known in advance
+
         flecs::world world;
 
-        typedef int Health;
+        typedef struct {int value;} Speed;
 
-        const int jarDlingHealth = 5;
-        auto jarDling = world.entity("Jar Dling").set<Health>(jarDlingHealth);
-        const int jebDarlsHealth = 3;
-        auto jebDarls = world.entity("Jeb Darls").set<Health>(jebDarlsHealth);
+        auto janeDoe = world.entity();
+        auto johnDoe = world.entity();
 
-        world.each([](Health& health) {
-            health -= 1;
+        const double janeDoeHealth = 5.32;
+        janeDoe.set<Health>({janeDoeHealth});
+        REQUIRE(janeDoe.has<Health>() == true);
+        REQUIRE(janeDoe.get<Health>()->value == janeDoeHealth);
+
+        const double johnDoeHealth = 3.76;
+        johnDoe.set<Health>({johnDoeHealth});
+        REQUIRE(johnDoe.has<Health>() == true);
+        REQUIRE(johnDoe.get<Health>()->value == johnDoeHealth);
+
+        const int janeDoeSpeed = 2;
+        janeDoe.set<Speed>({janeDoeSpeed});
+        REQUIRE(janeDoe.has<Speed>() == true);
+        REQUIRE(janeDoe.get<Speed>()->value == janeDoeSpeed);
+
+        const int johnDoeSpeed = 5;
+        johnDoe.set<Speed>({johnDoeSpeed});
+        REQUIRE(johnDoe.has<Speed>() == true);
+        REQUIRE(johnDoe.get<Speed>()->value == johnDoeSpeed);
+
+        world.each([](Health& health, Speed& speed) {
+            health.value -= 1;
+            speed.value += 1; 
         });
 
-        CHECK(*jarDling.get<Health>() == jarDlingHealth - 1);
-        CHECK(*jebDarls.get<Health>() == jebDarlsHealth - 1);
+        CHECK(janeDoe.get<Health>()->value == janeDoeHealth - 1);
+        CHECK(johnDoe.get<Health>()->value == johnDoeHealth - 1);
+        CHECK(janeDoe.get<Speed>()->value == janeDoeSpeed + 1);
+        CHECK(johnDoe.get<Speed>()->value == johnDoeSpeed + 1);
+
+        janeDoe.destruct();
+        johnDoe.destruct();
     }
 }
