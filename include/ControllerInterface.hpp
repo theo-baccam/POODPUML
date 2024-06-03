19fa6201895640d45537b6f3c4acbf7115f19224
theo-baccam <theo.baccam@laplateforme.io>
2024-06-03 12:26:30 +0200

    feat(ControllerInterface): added string for next controller

--- include/ControllerInterface.hpp
+++ include/ControllerInterface.hpp
@@ -1,9 +1,13 @@
 #pragma once
+#include <string>
 
 
 class ControllerInterface {
     public:
         ControllerInterface();
+
+        std::string nextControllerString;
+
         virtual ~ControllerInterface() = 0;
         virtual void run() = 0;
 };
