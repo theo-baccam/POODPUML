23ea2e9ff1d5e31c9c4d61cd3ff82f064fc31a69
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2024-06-10 14:56:04 +0200

    docs: add comments to AssetsLoader methods

--- src/AssetsLoader.cpp
+++ src/AssetsLoader.cpp
@@ -1,18 +1,21 @@
 #include <raylib.h>
 #include <AssetsLoader.hpp>
 
-
+// Constructor for the AssetsLoader class
 AssetsLoader::AssetsLoader() {
-    this->floorTileTexture = LoadTexture("assets/floor-tile.png");
-    this->pathTileTexture = LoadTexture("assets/path-tile.png");
-    this->startTexture = LoadTexture("assets/start.png");
-    this->endTexture = LoadTexture("assets/end.png");
-    this->cursorTexture = LoadTexture("assets/cursor.png");
+    // Load textures from the assets directory
+    this->floorTileTexture = LoadTexture("assets/floor-tile.png"); // Load floor tile texture
+    this->pathTileTexture = LoadTexture("assets/path-tile.png"); // Load path tile texture
+    this->startTexture = LoadTexture("assets/start.png"); // Load start texture
+    this->endTexture = LoadTexture("assets/end.png"); // Load end texture
+    this->cursorTexture = LoadTexture("assets/cursor.png"); // Load cursor texture
 }
+// Destructor for the AssetsLoader class
 AssetsLoader::~AssetsLoader() {
-    UnloadTexture(this->floorTileTexture);
-    UnloadTexture(this->pathTileTexture);
-    UnloadTexture(this->startTexture);
-    UnloadTexture(this->endTexture);
-    UnloadTexture(this->cursorTexture);
+    // Unload textures to free up memory
+    UnloadTexture(this->floorTileTexture); // Unload floor tile texture
+    UnloadTexture(this->pathTileTexture); // Unload path tile texture
+    UnloadTexture(this->startTexture); // Unload start texture
+    UnloadTexture(this->endTexture); // Unload end texture
+    UnloadTexture(this->cursorTexture); // Unload cursor texture
 }
