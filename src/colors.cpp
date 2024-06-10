508cc44a1d6b545ab324aeb75e8dffff1cacc293
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2024-06-10 14:59:02 +0200

    docs: add comment to colors.cpp

--- src/colors.cpp
+++ src/colors.cpp
@@ -2,6 +2,7 @@
 #include "colors.hpp"
 
 
+// Define color constants as Color objects from the raylib library
 Color COLOR0 = {3, 4, 25, 255};
 Color COLOR1 = {15, 18, 65, 255};
 Color COLOR2 = {47, 50, 103, 255};
